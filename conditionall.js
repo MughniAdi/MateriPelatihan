cbb30ca1cdb2801f858772f8fa0871e834b26eb3
MughniAdi <madinurhafiid@gmail.com>
2019-07-14 13:56:10 +0700

    Comit Yuk

---
+++ conditionall.js
@@ -0,0 +1,7 @@
+if (1 != 1){
+	console.log('1 tidak sama dengan 1');//kondisi pertama
+} else if (1 < 1){
+	console.log('1 tapi 1 lebih dari 0');//kondisi kedua
+} else {
+	console.log('ambil semua bro')//kondisi terakhir
+}
